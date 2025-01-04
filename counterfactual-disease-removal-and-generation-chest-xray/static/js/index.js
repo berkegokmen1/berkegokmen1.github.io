286cb87cf083c9dfa3551cbca60717315c15ae5a
Berke Gokmen <berkegokmen1@Berkes-MacBook-Pro.local>
2025-01-04 11:18:18 +0300

    website

--- counterfactual-disease-removal-and-generation-chest-xray/static/js/index.js
+++ counterfactual-disease-removal-and-generation-chest-xray/static/js/index.js
@@ -1,21 +1,28 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
+$(document).ready(function () {
+  // Check for click events on the navbar burger icon
 
-$(document).ready(function() {
-    // Check for click events on the navbar burger icon
+  var options = {
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    loop: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  };
 
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
-    }
+  // Initialize all div with carousel class
+  var carousels = bulmaCarousel.attach('.carousel', options);
 
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-	
-    bulmaSlider.attach();
+  bulmaSlider.attach();
+});
 
-})
+function toggleCollapse(id) {
+  const element = document.getElementById(id);
+  if (element.style.display === 'none' || element.style.display === '') {
+    element.style.display = 'flex';
+  } else {
+    element.style.display = 'none';
+  }
+}
